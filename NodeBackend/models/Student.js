9e40cc850ec28fa32a9c3f1c718fc64d3ca69f3a
ManeeshaMaduranga <MadurangaBandara1006@gmail.com>
2023-09-22 14:13:21 +0530

    Add Student Model

---
+++ NodeBackend/models/Student.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const StudentSchema = new mongoose.Schema({
+  userName: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  phoneNumber:{
+    type:string,
+  },
+  profilePicture: {
+    type: String,
+  }
+});
+
+const Student = mongoose.model('Student', StudentSchema);
+
+module.exports = {
+  Student,
+};
