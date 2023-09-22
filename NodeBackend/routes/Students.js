1152b34856e10945d16a9b6310c288fc787ff46e
ManeeshaMaduranga <MadurangaBandara1006@gmail.com>
2023-09-22 15:14:10 +0530

    Fix the Bug

--- NodeBackend/routes/Students.js
+++ NodeBackend/routes/Students.js
@@ -1,25 +1,25 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const bcrypt = require("bcrypt");
-const { Student } = require("../models/Student");
+const bcrypt = require('bcrypt');
+const { Student } = require('../models/Student');
 
-router.get("/", async (req, res) => {
+router.get('/', async (req, res) => {
   try {
     const students = await Student.find();
-    if (!students.length) return res.status(404).send("Students not found!");
+    if (!students.length) return res.status(404).send('Students not found!');
     res.status(200).json(students);
   } catch (ex) {
     console.log(ex);
-    res.status(400).send("Something went wrong!");
+    res.status(400).send('Something went wrong!');
   }
 });
 
-router.post("/", async (req, res) => {
+router.post('/', async (req, res) => {
   const salt = await bcrypt.genSalt(8);
   const password = await bcrypt.hash(req.body.password, salt);
 
   const student = await new Student({
-    userName: req.body.name,
+    userName: req.body.userName,
     email: req.body.email,
     phoneNumber: req.body.phoneNumber,
     profilePicture: req.body.profilePicture,
