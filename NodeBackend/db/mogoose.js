cee503197174bdeb9011ca4e1c837c12ed7f8887
Chamod Dananjaya <chamoddananjaya49@gmail.com>
2023-09-22 14:05:52 +0530

    Add db connection.

--- NodeBackend/db/mogoose.js
+++ NodeBackend/db/mogoose.js
@@ -1,1 +1,7 @@
-console.log('cONNECT DB');
+const mongoose = require("mongoose");
+const uri = process.env.DB_CONNECTION;
+
+mongoose
+  .connect(uri)
+  .then(() => console.log("Successfully connected to the DB."))
+  .catch((err) => console.log("Unable to connect with the DB."));
