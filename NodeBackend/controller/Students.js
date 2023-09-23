abd9b1f84f05f97f97820546037169265c41bc07
Chamod Dananjaya <chamoddananjaya49@gmail.com>
2023-09-23 14:11:33 +0530

    Add a controller for student routes.

---
+++ NodeBackend/controller/Students.js
@@ -0,0 +1,64 @@
+const bcrypt = require("bcrypt");
+const { Student } = require("../models/Student");
+
+export const getStudent = async (req, res) => {
+  const id = req.params.id;
+  const student = await Student.findByID(id);
+  if (!student) return res.stats(400).send("Student id is not valid!");
+
+  const { _id, userName, email, profilePicture, phoneNumber } = student;
+  res.status(200).json({ _id, userName, email, profilePicture, phoneNumber });
+};
+
+export const getStudents = async (req, res) => {
+  try {
+    const students = await Student.find();
+    if (!students.length) return res.status(404).send("Students not found!");
+    res.status(200).json(students);
+  } catch (ex) {
+    console.log(ex);
+    res.status(400).send("Something went wrong!");
+  }
+};
+
+export const addStudent = async (req, res) => {
+  const data = req.body;
+  const salt = await bcrypt.genSalt(8);
+  const password = await bcrypt.hash(data.password, salt);
+
+  const student = await new Student({
+    userName: data.userName,
+    email: data.email,
+    phoneNumber: data.phoneNumber,
+    profilePicture: data.profilePicture,
+    password,
+  }).save();
+
+  const { _id, userName, email, profilePicture, phoneNumber } = student;
+  res.status(201).json({ _id, userName, email, profilePicture, phoneNumber });
+};
+
+export const updateStudent = async (req, res) => {
+  const { userName, email, phoneNumber, profilePicture } = req.body;
+  const id = req.params.id;
+
+  const salt = await bcrypt.genSalt(8);
+  const password = await bcrypt.hash(data.password, salt);
+
+  const student = await Student.findByIdAndUpdate(
+    id,
+    { userName, email, phoneNumber, profilePicture, password },
+    { new: true }
+  ).select("-password");
+
+  if (!student)
+    return res.status(404).send("Student with given id is not found!");
+  res.status(200).json(student);
+};
+
+export const deleteStudent = async (req, res) => {
+  const id = req.params.id;
+  const student = await Student.findByIdAndDelete(id);
+  if (!student) return res.status(400).send("Student id is not valid!");
+  res.status(200).json(student);
+};
--- NodeBackend/routes/Students.js
+++ NodeBackend/routes/Students.js
@@ -1,33 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcrypt');
-const { Student } = require('../models/Student');
+const {
+  getStudents,
+  addStudent,
+  getStudent,
+  updateStudent,
+  deleteStudent,
+} = require("../controller/Students");
 
-router.get('/', async (req, res) => {
-  try {
-    const students = await Student.find();
-    if (!students.length) return res.status(404).send('Students not found!');
-    res.status(200).json(students);
-  } catch (ex) {
-    console.log(ex);
-    res.status(400).send('Something went wrong!');
-  }
-});
-
-router.post('/', async (req, res) => {
-  const salt = await bcrypt.genSalt(8);
-  const password = await bcrypt.hash(req.body.password, salt);
-
-  const student = await new Student({
-    userName: req.body.userName,
-    email: req.body.email,
-    phoneNumber: req.body.phoneNumber,
-    profilePicture: req.body.profilePicture,
-    password,
-  }).save();
-
-  const { _id, userName, email, profilePicture, phoneNumber } = student;
-  res.status(201).json({ _id, userName, email, profilePicture, phoneNumber });
-});
+router.get("/:id", getStudent);
+router.get("/", getStudents);
+router.post("/", addStudent);
+router.put("/:id", updateStudent);
+router.delete("/:id", deleteStudent);
 
 module.exports = router;
