3e05288ebe62bf51a43b31d87691865672a24de9
Chamod Dananjaya <chamoddananjaya49@gmail.com>
2023-09-23 14:47:00 +0530

    Add auth route.

--- NodeBackend/models/Student.js
+++ NodeBackend/models/Student.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
 
 const StudentSchema = new mongoose.Schema({
   userName: {
@@ -22,6 +23,21 @@ const StudentSchema = new mongoose.Schema({
     type: String,
   },
 });
+
+StudentSchema.methods.generateAuthToken = function () {
+  const token = jwt.sign(
+    {
+      _id: this._id,
+      name: this.userName,
+      email: this.email,
+      number: this.phoneNumber,
+      image: this.profilePicture,
+    },
+    process.env.JWT_PRIVATE_KEY
+  );
+
+  return token;
+};
 
 const Student = mongoose.model("Student", StudentSchema);
 
