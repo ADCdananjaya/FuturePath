2f2b1a5ea0ed9c20d73cf9ca3fb463fba95ab7e2
Chamod Dananjaya <chamoddananjaya49@gmail.com>
2023-09-23 16:03:06 +0530

    Add auth handler to server.js

--- NodeBackend/server.js
+++ NodeBackend/server.js
@@ -1,19 +1,21 @@
-const express = require('express');
-require('dotenv').config();
-require('./db/mongoose');
-const cors = require('cors');
+const express = require("express");
+require("dotenv").config();
+require("./db/mongoose");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
 
 //Routes
-const students = require('./routes/Students');
-const companyRoutes = require('./routes/Companies');
+const students = require("./routes/Students");
+const companyRoutes = require("./routes/Companies");
+const auth = require("./routes/AuthRoutes");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.use('/api/students', students);
-app.use('/api/company', companyRoutes);
+app.use("/api/students", students);
+app.use("/api/company", companyRoutes);
+app.use("/api/auth", auth);
 
 app.listen(port, () => console.log(`App listen on port ${port}!`));
